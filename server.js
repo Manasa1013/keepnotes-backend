6a1ba18d9db8d9ee7f686e57216e0268e9b90352
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2021-06-20 17:48:11 +0530

    docs : add content in response object to check

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const db_uri = process.env.DATABASE_URI;
 app.use(cors());
 
 app.get('/', (req, res) =>{ //this is a callback fn {
-    res.send(`Hello World from ${port}!`);//displays on port 3000 i.e.. url http://127.0.0.1:3000 or http://localhost:3000
+    res.send(`Hello keepnotes World from ${port}!`);
+    res.json({
+      taskList : [ 
+        {
+          id : 1,
+          title : "reading book",
+          task : "elaborating",
+        }
+      ]//displays on port 3000 i.e.. url http://127.0.0.1:3000 or http://localhost:3000
   });
 
 app.listen(port, function() {//this statement is to startup the server on port 3000
