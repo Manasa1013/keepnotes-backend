c72af97fc8d93b3171d7bd637a2a7390f332d001
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2021-06-20 17:57:39 +0530

    fix :  syntax error fixed

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ const db_uri = process.env.DATABASE_URI;
 app.use(cors());
 
 app.get('/', (req, res) =>{ //this is a callback fn {
-    res.send(`Hello keepnotes World from ${port}!`);
+    // res.send(`Hello keepnotes World from ${port}!`);
     res.json({
-      taskList : [ 
-        {
+       task : {
           id : 1,
           title : "reading book",
           task : "elaborating",
         }
-      ]//displays on port 3000 i.e.. url http://127.0.0.1:3000 or http://localhost:3000
+      //displays on port 3000 i.e.. url http://127.0.0.1:3000 or http://localhost:3000
   });
-
+})
 app.listen(port, function() {//this statement is to startup the server on port 3000
     console.log(`Example app listening on port ${port}`);//printed on console of that web page
     
