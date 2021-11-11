11a6888c56c67abe0a99ad7660ac421bd650dde2
Manasa Ramreddy Mandalreddy <66269691+Manasa1013@users.noreply.github.com>
2021-11-11 17:58:35 +0530

    remove : clarification comments

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const router  = express.Router();
- router.get("/" , (req,res) => {
-     res.json({taskList : [
-          {
-             id : 2,
-             title : "planning",
-             task : "elaborating"
-         }
-     ]})
- })
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.json({
+    taskList: [
+      {
+        id: 2,
+        title: "planning",
+        task: "elaborating"
+      }
+    ]
+  });
+});
 
 module.exports = router;
-//executed in cmd only
-//in backend ,reloading doesn't alter the changes made here in editor,you have to re-run it again.
--- server.js
+++ server.js
@@ -3,33 +3,35 @@ const cors = require("cors");
 const tasks = require("./routes/tasks");
 const bodyParser = require("body-parser");
 const app = express();
-const { initializeDBConnection }= require("./connection.mongo");
+const { initializeDBConnection } = require("./connection.mongo");
 initializeDBConnection();
 
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.urlencoded({
-  extended : true
-}));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(cors());
 
-app.use("/tasks" , tasks);
-app.get('/', (req, res) =>{ //this is a callback fn {
-    // res.send(`Hello keepnotes World from ${port}!`);
-    res.json({
-       task : {
-          id : 1,
-          title : "reading book",
-          task : "elaborating",
-        }
-      //displays on port 3000 i.e.. url http://127.0.0.1:3000 or http://localhost:3000
+app.use("/tasks", tasks);
+app.get("/", (req, res) => {
+  res.json({
+    task: {
+      id: 1,
+      title: "reading book",
+      task: "elaborating"
+    }
+    //displays on port 3000 i.e.. url http://127.0.0.1:3000 or http://localhost:3000
   });
-})
-app.listen(port, () => {//this statement is to startup the server on port 3000
-    console.log(`Example app listening on port ${port}`);//printed on console of that web page
-    
-  });
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
